d5605cdbdcc233bce2ef5976f8f84d9f8f7d48fd
unknown <David Hernandez@CAA151.albernet.local>
2022-12-20 15:27:10 -0500

    Modificacion en rutas de carpetas 2 para produccion

--- app/Http/Controllers/MoveFileController.php
+++ app/Http/Controllers/MoveFileController.php
@@ -26,7 +26,7 @@ class MoveFileController extends Controller
         // $destination = "\\\\10.1.1.82\Simi\pdf\\Estados\\EstadosCuentaEnviados\\{$currentYear}\\{$currentMonth}\\{$todayDate}";
 
         // Para producción
-        $destination = "//mnt//server//EstadosCuentaEnviados//{$currentYear}//{$currentMonth}//{$todayDate}";
+        $destination = "/mnt/server/EstadosCuentaEnviados/{$currentYear}/{$currentMonth}/{$todayDate}";
 
         try {
             //Validar si la carpeta existe con fecha actual
