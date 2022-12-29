75d95752c82863149b94259437b2b39b8fa32fd4
unknown <David Hernandez@CAA151.albernet.local>
2022-12-29 15:00:18 -0500

    Plantilla html modificada

--- resources/views/EstadosCuenta/EstadoCuenta.blade.php
+++ resources/views/EstadosCuenta/EstadoCuenta.blade.php
@@ -16,7 +16,7 @@
             <td>
                 <p>Medellín, </p>
                 <p>Cordial Saludo,</p>
-                <p>Le informamos que el estado de cuenta de su inmueble, correspondiente al mes de Diciembre ya se encuentra disponible.</p>
+                <p>Le informamos que el estado de cuenta de su inmueble ya se encuentra disponible.</p>
                 <p>Para consultarlo descargue el archivo adjunto en este correo el cual puede abrir con el número de cédula o NIT del titular. Tambien puede ingresar a www.albertoalvarez.com, haga clic en el bot&oacute;n propietarios, digite usuario y clave, luego seleccione la fecha de corte que desea visualizar.</p>
                 <p>Si tiene alguna inquietud, no dude en contactarnos a través de nuestra línea telefónica: 444 98 98 Opción 1.</p>
                 <p>Si necesita algun documento adicional puede solicitarlo a través de nuestro correo electrónico info@albertoalvarez.com</p>
